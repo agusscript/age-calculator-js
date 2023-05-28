70185491e1a12ac6ca3f0e94d71ee8b298344722
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-05-27 23:31:11 -0300

    Add function to calculate and show user age

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 const $inputDay = document.querySelector(".input-day");
 const $inputMonth = document.querySelector(".input-month");
 const $inputYear = document.querySelector(".input-year");
-
-const $labelDay = document.querySelector(".label-day");
-const $labelMonth = document.querySelector(".label-month");
-const $labelYear = document.querySelector(".label-year");
-
-const $btnCalculate = document.querySelector(".btn-calculate");
+const $buttonCalculate = document.querySelector(".btn-calculate");
 
 function validateForm() {
-  console.log($inputDay.value);
+  const $labelDay = document.querySelector(".label-day");
+  const $labelMonth = document.querySelector(".label-month");
+  const $labelYear = document.querySelector(".label-year");
+}
+
+function showUserAge() {
+  const currentDate = new Date().toJSON().slice(0, 10);
+  const currentYear = parseInt(currentDate.slice(0, 4));
+  const currentMonth = parseInt(currentDate.slice(5, 7));
+  const currentDay = parseInt(currentDate.slice(8, 10));
+  let userYears = currentYear - $inputYear.value;
+  let userMonths = currentMonth - $inputMonth.value;
+  const userDays = currentDay - $inputDay.value;
+
+  if ($inputMonth.value > currentMonth) {
+    --userYears;
+    userMonths += 12;
+  }
+
+  document.querySelector(".text-year").textContent = userYears;
+  document.querySelector(".text-month").textContent = userMonths;
+  document.querySelector(".text-day").textContent = userDays;
 
   event.preventDefault();
 }
 
-$btnCalculate.onclick = validateForm;
+$buttonCalculate.onclick = showUserAge;
