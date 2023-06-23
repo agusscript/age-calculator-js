40c1460e2850cd19d967e72af9e3514941f80e1e
agusscript <agus.sanchez.240@gmail.com>
2023-06-22 21:20:05 -0300

    Add types to variables and functions

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-const $form = document.querySelector(".age-form");
-const $inputDay = document.querySelector(".input-day");
-const $inputMonth = document.querySelector(".input-month");
-const $inputYear = document.querySelector(".input-year");
-const $buttonCalculate = document.querySelector(".btn-calculate");
-const date = new Date();
-let currentYear = date.getFullYear();
-let currentMonth = date.getMonth() + 1;
-let currentDay = date.getDate();
-let errorDay = 0;
-let errorMonth = 0;
-let errorYear = 0;
+const $form: any = document.querySelector(".age-form");
+const $inputDay: any = document.querySelector(".input-day");
+const $inputMonth: any = document.querySelector(".input-month");
+const $inputYear: any = document.querySelector(".input-year");
+const $buttonCalculate: any = document.querySelector(".btn-calculate");
+let errorDay: boolean = false;
+let errorMonth: boolean = false;
+let errorYear: boolean = false;
 
-const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const months: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function showUserAge() {
+function showUserAge(): void {
+  const date: Date = new Date();
+  let currentYear: number = date.getFullYear();
+  let currentMonth: number = date.getMonth() + 1;
+  let currentDay: number = date.getDate();
+
   if ($inputDay.value > currentDay) {
     currentDay = currentDay + months[currentMonth - 1];
     currentMonth = currentMonth - 1;
@@ -28,89 +29,96 @@ function showUserAge() {
   let userMonths = currentMonth - $inputMonth.value;
   const userDays = currentDay - $inputDay.value;
 
-  document.querySelector(".text-year").textContent = userYears;
-  document.querySelector(".text-month").textContent = userMonths;
-  document.querySelector(".text-day").textContent = userDays;
+  const $textYear: any = document.querySelector(".text-year");
+  const $textMonth: any = document.querySelector(".text-month");
+  const $textDay: any = document.querySelector(".text-day");
 
-  event.preventDefault();
+  $textYear.textContent = userYears;
+  $textMonth.textContent = userMonths;
+  $textDay.textContent = userDays;
+
+  event?.preventDefault();
 }
 
-function validateDay(DD) {
-  const $labelDay = document.querySelector(".label-day");
-  const $textErrorDay = document.querySelector(".day-error-text");
+function validateDay(DD: string): boolean {
+  const $labelDay: any = document.querySelector(".label-day");
+  const $textErrorDay: any = document.querySelector(".day-error-text");
 
   if (DD.length === 0) {
     $textErrorDay.textContent = "This field is required";
     $labelDay.classList.add("error");
     $inputDay.classList.add("error");
-    return errorDay++;
+    return (errorDay = true);
   } else if (!/^(0?[1-9]|[12][0-9]|3[01])$/.test(DD)) {
     $textErrorDay.textContent = "Invalid Day format";
     $labelDay.classList.add("error");
     $inputDay.classList.add("error");
-    return errorDay++;
+    return (errorDay = true);
   } else {
     $textErrorDay.textContent = "";
     $labelDay.classList.remove("error");
     $inputDay.classList.remove("error");
-    return (errorDay = 0);
+    return (errorDay = false);
   }
 }
 
-function validateMonth(MM) {
-  const $labelMonth = document.querySelector(".label-month");
-  const $textErrorMonth = document.querySelector(".month-error-text");
+function validateMonth(MM: string): boolean {
+  const $labelMonth: any = document.querySelector(".label-month");
+  const $textErrorMonth: any = document.querySelector(".month-error-text");
 
   if (MM.length === 0) {
     $textErrorMonth.textContent = "This field is required";
     $labelMonth.classList.add("error");
     $inputMonth.classList.add("error");
-    return errorMonth++;
+    return (errorMonth = true);
   } else if (!/^0[0-9]|1[0-2]$/.test(MM)) {
     $textErrorMonth.textContent = "Invalid month format";
     $labelMonth.classList.add("error");
     $inputMonth.classList.add("error");
-    return errorMonth++;
+    return (errorMonth = true);
   } else {
     $textErrorMonth.textContent = "";
     $labelMonth.classList.remove("error");
     $inputMonth.classList.remove("error");
-    return (errorMonth = 0);
+    return (errorMonth = false);
   }
 }
 
-function validateYear(YYYY) {
-  const $labelYear = document.querySelector(".label-year");
-  const $textErrorYear = document.querySelector(".year-error-text");
+function validateYear(YYYY: string): boolean {
+  const $labelYear: any = document.querySelector(".label-year");
+  const $textErrorYear: any = document.querySelector(".year-error-text");
+  const currentYear = new Date().getFullYear();
+  const userYear: number = Number(YYYY);
 
   if (YYYY.length === 0) {
     $textErrorYear.textContent = "This field is required";
     $labelYear.classList.add("error");
     $inputYear.classList.add("error");
-    return errorYear++;
-  } else if (YYYY.length != 4 || YYYY > currentYear) {
+    return (errorYear = true);
+  } else if (userYear > currentYear) {
     $textErrorYear.textContent = "Invalid year format";
     $labelYear.classList.add("error");
     $inputYear.classList.add("error");
-    return errorYear++;
+    return (errorYear = true);
   } else {
     $textErrorYear.textContent = "";
     $labelYear.classList.remove("error");
     $inputYear.classList.remove("error");
-    return (errorYear = 0);
+    return (errorYear = false);
   }
 }
 
-function validateForm() {
+function validateForm(): void {
   validateMonth($inputMonth.value);
   validateDay($inputDay.value);
   validateYear($inputYear.value);
 
-  if (errorDay == 0 && errorMonth == 0 && errorYear == 0) {
+  if (errorDay == false && errorMonth == false && errorYear == false) {
     showUserAge();
   }
 
-  event.preventDefault();
+  console.log(event?.preventDefault());
+  event?.preventDefault();
 }
 
 $buttonCalculate.onclick = validateForm;
